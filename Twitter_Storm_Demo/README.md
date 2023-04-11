b5e472b201f77b878f87b12f5edd53327a53c2d6
bblinder <filmmaven@gmail.com>
2023-04-10 20:29:18 -0400

    modified:   README.md 	modified:   Twitter_Storm.py

--- Twitter_Storm_Demo/README.md
+++ Twitter_Storm_Demo/README.md
@@ -1,22 +1,21 @@
-# Twitter Storm demo
+# Twitter Storm Demo
 
-A flask app that fires off a list of pre-written "tweets" as custom events. Overlay with the event feed in the CICD demo for best effect.
+A Flask app that fires off a list of pre-written "tweets" as custom events. Overlay with the event feed in the CICD demo for best effect.
 
-The script can be run locally or in a cloud instance. Update the [canary deployment script](https://cd.splunkdev.com/observability-sales-engineering/field-demos/-/blob/main/CICD%20Automation%20Demo/Readme.md) to include a `curl` to the flask endpoint.
+The script can be run locally or in a cloud instance. Update the [canary deployment script](https://cd.splunkdev.com/observability-sales-engineering/field-demos/-/blob/main/CICD%20Automation%20Demo/Readme.md) to include a `curl` to the Flask endpoint.
 
-## Pre-work
+## Prerequisites
 
 1. Retrieve your O11y cloud/SignalFx token. **Ensure it has ingest permissions.**
-1. Edit the `Tweets.txt` file to include the tweets you want to send (**one per line**). Some examples are included.
-2. Run `pip install -r requirements.txt` to ensure dependencies are met.
-3. Include `SIGNALFX_ORG_ACCESS_TOKEN` and `REALM` in either the `.env` file or as environmental variables
-    - Ex: `export SIGNALFX_ORG_ACCESS_TOKEN=xxxx`
+2. Edit the `Tweets.txt` file to include the tweets you want to send (**one per line**). Some examples are included.
+3. Run `pip install -r requirements.txt` to ensure dependencies are met.
+4. Set the `SIGNALFX_ORG_ACCESS_TOKEN` and `REALM` environment variables, or include them in the `.env` file.
+    - Example: `export SIGNALFX_ORG_ACCESS_TOKEN=xxxx`
 
+## Running the Demo
 
-## Running the demo
-
-1. Run `python3 Twitter_Storm.py` to kick off the script.
-     - If running from a cloud instance, it's recommended you use `nohup` to separate the running script from your terminal process. This ensures that it continues running if you lose connection to your instance.
+1. Run `python3 Twitter_Storm.py` to start the script.
+   - If running from a cloud instance, it's recommended you use `nohup` to separate the running script from your terminal process. This ensures that it continues running if you lose connection to your instance.
 2. Note the IP and port it exposes (usually `127.0.0.1:8080`), though this can be a public-facing cloud instance.
 3. Open the URL in a browser, or use a GET request (`curl http://{URL_OF_ENDPOINT}:8080`) to trigger the tweet storm.
    - Optional: include the curl request as a line in the `canary.sh` script so it triggers as part of the CICD demo.
--- Twitter_Storm_Demo/Twitter_Storm.py
+++ Twitter_Storm_Demo/Twitter_Storm.py
@@ -1,93 +1,112 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import random
-import requests
+"""
+Variables to set:
+    REALM: The SignalFx realm to use (e.g. us0, us1, etc.) Defaults to us0.
+    SIGNALFX_ORG_ACCESS_TOKEN: The SignalFx organization access token.
+
+Variables should be set in the .env file or as environment variables.
+Don't hardcode them in the script.
+
+Example:
+    `export REALM=us0`
+    `export ORG_ACCESS_TOKEN=<SignalFx Org Access Token>`
+"""
+
 import os
+import random
 import sys
 from time import sleep
-from flask import Flask
+
+import requests
 from dotenv import load_dotenv
-
-load_dotenv()  # load environment variables
-
-# Set realm and org_access_token in either the '.env' file or as environment variables
-realm = os.getenv('REALM')  # change to reflect your SignalFx Realm
-org_access_token = os.getenv('SIGNALFX_ORG_ACCESS_TOKEN')
+from flask import Flask
 
 app = Flask(__name__)
 
 
+def load_config():
+    """
+    Load the realm and org access token from environment variables.
+    Defaults the realm to us0 if no realm is set.
+    """
+    load_dotenv()  # load environment variables
+    realm = os.getenv(
+        "REALM", "us0"
+    )  # change to reflect your SignalFx Realm. Defaults to us0.
+    org_access_token = os.getenv("SIGNALFX_ORG_ACCESS_TOKEN")
+    return realm, org_access_token
+
+
 def generate_username():
     """
-    Generates a random username for the tweet.
+    Generates a random user name from the words.txt file,
+    and appends a random number to the end.
     """
-    with open('words.txt', 'r') as f:
-        words = f.readlines()
-        words = [word.strip() for word in words]
-        random_number = random.randint(0, len(words) - 1)
-        username = words[random_number] + str(random_number)
-    return username
+    with open("words.txt", "r") as f:
+        words = [word.strip() for word in f.readlines()]
+        return random.choice(words) + str(random.randint(0, len(words) - 1))
 
 
-# turn Tweets.txt into a list
 def read_tweets():
     """
-    Reads the tweets from the file and turns them into a list.
-    It turns them into single quotes for now, as for some reason
-    the SignalFx ingest API doesn't like double quotes.
+    Reads the tweets from the Tweets.txt file, strips out apostrophes,
+    and converts them to single quotes.
     """
-    with open('Tweets.txt', 'r') as f:
-        list_of_tweets = f.readlines()
-        # strip out apostrophes and convert to single quotes
-        list_of_tweets = [tweet.strip("'").replace("'", "") for tweet in list_of_tweets]
-    return list_of_tweets
+    with open("Tweets.txt", "r") as f:
+        return [tweet.strip("'").replace("'", "") for tweet in f.readlines()]
 
 
-def send_tweet():
+def send_tweet(realm, org_access_token):
     """
-    Iterates through the list of tweets and sends them as custom events.
-    Be sure to change the realm to reflect your SignalFx Realm (ex: us0, us1, etc.)
+    Sends the tweets as custom events to the SignalFx ingest API.
+
+    Args:
+        realm (str): The SignalFx realm to use (e.g. us0, us1, etc.) Defaults to us0.
+        org_access_token (str): The SignalFx organization access token.
+
     """
-    list_of_tweets = read_tweets()
+    tweets = read_tweets()
     username = generate_username()
-    for t in list_of_tweets:
-        tweet = "twitter: " + t + " - " + "@" + username
+    endpoint = f"https://ingest.{realm}.signalfx.com/v2/event"
+    headers = {"Content-Type": "application/json", "X-SF-TOKEN": org_access_token}
 
-        # SignalFx
-        endpoint = f'https://ingest.{realm}.signalfx.com/v2/event'
-
-        # Set headers
-        headers = {
-            'Content-Type': 'application/json',
-            'X-SF-TOKEN': org_access_token
-        }
-
-        data = [{
-            "category": "USER_DEFINED",
-            "eventType": tweet,
-            "dimensions": {
-                "feed": "twitter"
+    for tweet_text in tweets:
+        tweet = f"twitter: {tweet_text} - @{username}"
+        data = [
+            {
+                "category": "USER_DEFINED",
+                "eventType": tweet,
+                "dimensions": {"feed": "twitter"},
             }
-        }]
-
+        ]
         print(data)
-        r = requests.post(endpoint, headers=headers, json=data)
-        sleep(1)  # sending tweets every 1 second.
-        # putting here for debugging purposes. Useful to see if you're getting any 'unauthorized' errors.
-        print(r.status_code)
+        with requests.post(url=endpoint, headers=headers, json=data) as response:
+            sleep(1)  # send one tweet per second
+            print(response.status_code)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    send_tweet()
+    """
+    Flask route for the root endpoint. Sends the tweets and returns an HTML response.
+
+    Returns:
+        str: An HTML string indicating the tweets have been sent.
+    """
+    realm, org_access_token = load_config()
+    send_tweet(realm, org_access_token)
     return "<h1>Tweets sent!</h1>"
 
 
-# don't run if org_access_token or realm is not set
-if org_access_token is None or realm is None:
-    print("You must set the org_access_token and realm.") 
-    print("This can be done in the '.env' file or as environment variables.")
-    sys.exit(1)
-else:
-    app.run(host="0.0.0.0", port="8080")  # include "debug=True" if you need troubleshooting
+if __name__ == "__main__":
+    realm, org_access_token = load_config()
+    if org_access_token is None or realm is None:
+        print("You must set the ORG_ACCESS_TOKEN and REALM.")
+        print("This can be done in the '.env' file or as environment variables.")
+        sys.exit(1)
+    else:
+        app.run(
+            host="0.0.0.0", port="8080"
+        )  # include "debug=True" if you need troubleshooting
